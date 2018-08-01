c6f22869d8a35bf56a2c2f5e1f707e1ea1de559b
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-01 11:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var recipes = new Object {};
+var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({},object);
+  newObj.key = value;
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object.key = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({},object);
+  delete newObj.key;
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectWithKey(object, key){
+  delete object.key;
   return object;
 }
